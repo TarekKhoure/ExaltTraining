1d1038d43bf8076a4ffc27e2a22ba05918e24254
Tarek Khoury <TarekK@exalt.ps>
2021-07-11 09:19:23 +0300

    first push

---
+++ item-app.js
@@ -0,0 +1,47 @@
+/*
+ * showLoader to display loader when page is still loading and hideLoader 
+ * to hide loader when page is ready
+ */
+function showLoader() {
+    $('.loader').css('display', 'flex')
+}
+
+function hideLoader() {
+    $('.loader').hide();
+}
+
+
+$(document).ready(function () {
+    showLoader();
+    $.get("https://jsonplaceholder.typicode.com/todos/", function (data, status) {
+        hideLoader();
+
+            $(".row1").append(`<div class='item-container' productId=${data[i].id} id=item_${i}> \
+            <div class = 'item-image-container'> \
+            <div class= 'item-image'></div>\
+            </div>\
+            <p>★★★★☆</p> \
+            <div class ='last-section' id='test_${i}'></div>\
+            </div>`);
+
+            var para = document.createElement("p");
+            para.className = "description";
+            var node = document.createTextNode(data[i].title);
+            para.appendChild(node);
+            const element = document.getElementById("test_" + i);
+            element.appendChild(para);
+
+
+            // create p element to fill id in
+            const para1 = document.createElement("p");
+            para1.className = "price";
+            const node1 = document.createTextNode("Price: " + data[i].id + "$");
+            para1.appendChild(node1);
+            const element1 = document.getElementById(`test_${i}`);
+            element1.appendChild(para1);
+    
+    });
+
+});
+
+
